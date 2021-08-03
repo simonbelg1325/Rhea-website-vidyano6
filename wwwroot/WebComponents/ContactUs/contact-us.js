bd12e68f6294002d94f37bda117e7b4d8de51aa1
Simon Wondimu <simon.wondimu@cronos.be>
2021-08-03 09:40:09 +0200

    UpdateOnContactPage

--- wwwroot/WebComponents/ContactUs/contact-us.js
+++ wwwroot/WebComponents/ContactUs/contact-us.js
@@ -9,7 +9,23 @@ var Rhea;
                 return _super !== null && _super.apply(this, arguments) || this;
             }
             ContactUs.prototype.attached = function () {
-                _super.prototype.attached.call(this);
+                return __awaiter(this, void 0, void 0, function () {
+                    var query;
+                    return __generator(this, function (_a) {
+                        switch (_a.label) {
+                            case 0:
+                                _super.prototype.attached.call(this);
+                                return [4, this.app.initialize];
+                            case 1:
+                                _a.sent();
+                                return [4, this.service.getQuery("GetContactInfo")];
+                            case 2:
+                                query = _a.sent();
+                                this._setContactInfo(query);
+                                return [2];
+                        }
+                    });
+                });
             };
             ContactUs = __decorate([
                 Vidyano.WebComponents.WebComponent.register({
@@ -17,6 +33,11 @@ var Rhea;
                         contact: {
                             type: Boolean,
                             value: true
+                        },
+                        contactInfo: {
+                            type: Object,
+                            readOnly: true,
+                            notify: true
                         }
                     }
                 }, "rhea")
--- wwwroot/WebComponents/ContactUs/contact-us.ts
+++ wwwroot/WebComponents/ContactUs/contact-us.ts
@@ -4,13 +4,23 @@
             contact: {
                 type: Boolean,
                 value: true
+            },
+            contactInfo: {
+                type: Object,
+                readOnly: true,
+                notify: true
             }
         }
     }, "rhea")
 
     export class ContactUs extends Vidyano.WebComponents.WebComponent {
-        attached() {
+        contactInfo: Vidyano.Query; private _setContactInfo: (value: Vidyano.Query) => void;
+
+        async attached() {
             super.attached();
+            await this.app.initialize;
+            const query = await this.service.getQuery("GetContactInfo");
+            this._setContactInfo(query);
         }
     }
 }
